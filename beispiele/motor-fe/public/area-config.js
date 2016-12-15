5dee9bc2d5cef4f68590e02fc6668cb27aa5c90a
Matthias Hermsdorf <matthiashermsdorf@gmx.de>
2016-12-15 23:11:49 +0100

    optisches Feedback wenn der Server gestoppt wird

--- beispiele/motor-fe/public/area-config.js
+++ beispiele/motor-fe/public/area-config.js
@@ -4,14 +4,22 @@ $(function () {
     //socket.emit("drive", "msg");
     $("[data-reboot]").on("click", function () {
         socket.emit("reboot","");
+        die();
     })
 
     $("[data-shutdown]").on("click", function () {
         socket.emit("shutdown","");
+        die();
     })
 
     $("[data-quit-server]").on("click", function () {
         socket.emit("quit-server","");
+        die();
     })
+
+    function die () {
+        $("body").addClass("dead");
+        $(".area-buttons, .area-control, .area-config").fadeOut();
+    }
 
 });
